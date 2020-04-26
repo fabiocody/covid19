41d44382fcf3a98c51835052526f3dd21e704818
Fabio Codiglioni <fabiocody@icloud.com>
2020-04-26 19:16:34 +0200

    Update

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 source env/bin/activate
 
 jupyter nbconvert --to notebook --inplace --execute covid19.ipynb
+jupyter nbconvert --to notebook --inplace --execute covid19-lombardia.ipynb
 git commit -am "Update"
 git push
